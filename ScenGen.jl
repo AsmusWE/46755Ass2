51d33a5652eeabc3fe3e5f615d6adef20a03f8a7
Viktor-Johnsen <s203649@student.dtu.dk>
2024-04-19 10:10:32 +0200

    Step 1.1 and 1.2

--- ScenGen.jl
+++ ScenGen.jl
@@ -1,4 +1,6 @@
 using CSV, DataFrames
+using Random
+
 function GenScens()
     priceDF = CSV.read("data/Elspotprices.csv", DataFrame, decimal=',')
     prodDF = CSV.read("data/Forecasts_Hour.csv", DataFrame, decimal=',')
@@ -7,6 +9,7 @@ function GenScens()
     prodDays = 20
     imbalanceScens = 3
     imbalance = zeros(3,24)
+    Random.seed!(1234)
     for i in 1:3
         imbalance[i,:] = rand(Bool, 24)
     end
@@ -17,9 +20,9 @@ function GenScens()
         for j in range(1, prodDays)
             for u in range(1, imbalanceScens)
                 # Setting price
-                scenarios[currScen, :, 1] = priceDF[i:(i+23),5]
+                scenarios[currScen, :, 1] = priceDF[i:(i+23),5] #column 5 is spot price in EU/MWh
                 # Setting prod
-                scenarios[currScen, :, 2] = prodDF[j:(j+23),9]
+                scenarios[currScen, :, 2] = prodDF[j:(j+23),9] #column 9 is current forecast in MWh/h
                 # Setting deficit
                 scenarios[currScen, :, 3] = imbalance[u,:]
                 # Advancing scenarios
---
+++ Step1_1.jl
@@ -0,0 +1,48 @@
+using JuMP, Gurobi
+include("ScenGen.jl")
+scenarios = GenScens() #scenarios, t, price prod imbalance
+
+lambda_DA = scenarios[:,:,1]
+p_real = scenarios[:,:,2] #COMMENT: should be normalized to the 200 MW wind farm size in the assignment!!
+Imbalance = scenarios[:,:,3]
+
+T = collect(1:size(scenarios)[2])
+W = collect(1:size(scenarios)[1])
+
+prob = ones(W[end])/W[end] #COMMENT: should not be all scenarios but only 250 out of 1200!!
+P_nom = 200 #MW
+
+
+
+#************************************************************************
+# MODEL
+Step1_1 = Model(Gurobi.Optimizer)
+
+@variable(Step1_1, 0 <= p_DA[t in T] <= P_nom) #Electricity offered in DA market
+@variable(Step1_1, delta_t[w in W, t in T]) #Realised difference in generation and offer in DA, auxiliary
+@variable(Step1_1, I_B[w in W, t in T]) #The profit balance in balancing market
+
+@objective(Step1_1, Max,
+            sum( prob[w] * sum( lambda_DA[w,t]*p_DA[t] + I_B[w,t] for t in T) for w in W))
+
+@constraint(Step1_1, [w in W, t in T],
+            delta_t[w,t] == p_real[w,t] - p_DA[t])
+@constraint(Step1_1, [w in W, t in T],
+            I_B[w,t] <= (Imbalance[w,t]*0.9 + (1-Imbalance[w,t])*1.1) * lambda_DA[w,t] * delta_t[w,t])
+#************************************************************************
+
+#************************************************************************
+# SOLVE
+set_time_limit_sec(Step1_1,30)
+solution = optimize!(Step1_1)
+println("Termination status: $(termination_status(Step1_1))")
+#************************************************************************
+
+#************************************************************************
+# SOLUTION
+if termination_status(Step1_1) == MOI.OPTIMAL
+    println("RESULTS:")
+    printstyled("objective = $(objective_value(Step1_1))\n";color= :blue)
+end
+#************************************************************************
+
